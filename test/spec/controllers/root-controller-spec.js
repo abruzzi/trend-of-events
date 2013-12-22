9cff122d97c03aa526f73d4e9b7d87df8b1d053e
Qiu Juntao <juntao.qiu@gmail.com>
2013-12-22 17:03:40 +1100

    add controller test

---
+++ test/spec/controllers/root-controller-spec.js
@@ -0,0 +1,30 @@
+define(['angular', 
+        'angular-mocks',
+        'controllers/controllers',
+        'controllers/root-controller'], 
+        function() {
+            describe("Root Controller", function() {
+                var scope, controller;
+
+                beforeEach(function() {
+                    module('controllers');
+                    inject(function($rootScope, $controller) {
+                        scope = $rootScope.$new();
+                        controller = $controller('RootController', {$scope: scope});
+                    });
+                });
+
+                it("should have a title", function() {
+                    expect(scope.title).toEqual("This is my chart");
+                });
+
+                it("should hidden", function() {
+                    expect(scope.isHidden).toBeTruthy();
+                });
+
+                it("should toggle hidden", function() {
+                    scope.toggleIt();
+                    expect(scope.isHidden).toBeFalsy();
+                })
+            }); 
+});
