4e5d30e2faa57be8f1ecfb23a7afc87f95aa864c
Qiu Juntao <juntao.qiu@gmail.com>
2013-12-24 01:08:03 +1100

    add e2e scripts and a scenario

---
+++ test/e2e/root-spec.js
@@ -0,0 +1,17 @@
+'use strict';
+
+describe("index page", function() {
+    beforeEach(function() {
+        browser().navigateTo("/app/index.html");
+        sleep(1);
+    });
+
+    it("should have navigator", function() {
+        expect(element("#navigator")).toBeDefined();
+    });
+
+    it("have events path", function() {
+        expect(browser().location().path()).toBe("/");
+    });
+});
+
